4f66aac44540440f1b5d28fbe0efc55d30c45ed5
IMBABOT1 <zlovred12@gmail.com>
2022-01-01 14:37:50 +0300

    findMin

--- src/main/java/ru/geekbrains/Algorithms/lesson_one/Main.java
+++ src/main/java/ru/geekbrains/Algorithms/lesson_one/Main.java
@@ -2,7 +2,28 @@ package ru.geekbrains.Algorithms.lesson_one;
 
 public class Main {
 
+    //O(n)
+    private static int degree(int number, int degree){
+        int result = 1;
+
+        for (int i = 0; i < degree; i++) {
+            result *= number;
+        }
+
+        return result;
+    }
+
+    private static int findMin(int... arr){
+        int min = arr[0];
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(findMin(0, -1223, 8, 3, -567));
     }
 }
